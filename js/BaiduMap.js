5482bd31ced1d5a4a201d0c44073302bb61d4d0c
wsadwsda1727dab <1513212149@qq.com>
2023-07-08 19:53:24 +0800

    s

--- js/BaiduMap.js
+++
@@ -1,5 +0,0 @@
-function into(){
-		var map = new BMapGL.Map('l-map'); // 创建Map实例
-		map.centerAndZoom(new BMapGL.Point(114.31791,30.548469), 13); // 初始化地图,设置中心点坐标和地图级别
-		map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
-};
